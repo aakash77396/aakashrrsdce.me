99645872e7d8ff371b53448add40f20c09265ce1
Aakash Kumar <122362592+aakash77396@users.noreply.github.com>
2023-11-02 19:04:39 +0530

    Add files via upload
    Signed-off-by: Aakash Kumar <122362592+aakash77396@users.noreply.github.com>

---
+++ BubbleScript.js
@@ -0,0 +1,54 @@
+
+function makeBubble() {
+    let newBubble = "";
+
+    for (let i = 1; i <= 170; i++) {
+        let randomNum = Math.floor(Math.random() * 10);
+        newBubble += `<div id="bubble" onclick="hitNo(1)">${randomNum}</div>`;
+        console.log("hello");
+    }
+
+    document.querySelector("#downPart").innerHTML = newBubble;
+}
+
+let sec = 60;
+function runTimer() {
+    let timer = setInterval(function () {
+        if (sec > 0) {
+            sec--;
+            document.querySelector("#timer").innerHTML = sec;
+        }
+        else {
+            clearInterval(timer);
+            document.querySelector("#downPart").innerHTML = "<h1 id = 'gOver'> Game Over.... </h1>"
+        }
+    }, "1000");
+
+}
+
+let hitRandom;
+function getNewhit() {
+    hitRandom = Math.floor(Math.random() * 10);
+    document.querySelector("#hit").textContent = hitRandom;
+}
+
+let score = 0;
+function increaseScore() {
+    score += 10;
+    document.querySelector("#score").textContent = score;
+}
+
+document.querySelector("#downPart").addEventListener("click",function(details){
+    let clickedNumber = Number(details.target.textContent);
+    if(clickedNumber === hitRandom){
+        increaseScore();
+        makeBubble();
+        getNewhit();
+    }
+});
+
+
+
+makeBubble();
+runTimer();
+getNewhit();
